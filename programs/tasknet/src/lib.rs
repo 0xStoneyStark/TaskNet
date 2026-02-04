98fe34ba2319f8f207342388af9fbbcb91859b71
Jarvis <jarvis@agents.local>
2026-02-04 14:49:37 +0000

    Add claim_task instruction

--- programs/tasknet/src/lib.rs
+++ programs/tasknet/src/lib.rs
@@ -10,6 +10,15 @@ pub mod tasknet {
         let task = &mut ctx.accounts.task;
         task.creator = ctx.accounts.creator.key();
         task.status = TaskStatus::Created;
+        task.claimant = None;
+        Ok(())
+    }
+
+    pub fn claim_task(ctx: Context<ClaimTask>) -> Result<()> {
+        let task = &mut ctx.accounts.task;
+        require!(task.status == TaskStatus::Created, TaskError::AlreadyClaimed);
+        task.claimant = Some(ctx.accounts.claimer.key());
+        task.status = TaskStatus::Claimed;
         Ok(())
     }
 }
@@ -23,17 +32,32 @@ pub struct CreateTask<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct ClaimTask<'info> {
+    #[account(mut)]
+    pub task: Account<'info, Task>,
+    pub claimer: Signer<'info>,
+}
+
 #[account]
 pub struct Task {
     pub creator: Pubkey,
+    pub claimant: Option<Pubkey>,
     pub status: TaskStatus,
 }
 
 impl Task {
-    pub const SIZE: usize = 32 + 1;
+    pub const SIZE: usize = 32 + 1 + 32 + 1;
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
 pub enum TaskStatus {
     Created,
+    Claimed,
+}
+
+#[error_code]
+pub enum TaskError {
+    #[msg("Task already claimed")]
+    AlreadyClaimed,
 }
